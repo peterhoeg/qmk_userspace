9cef27cbac44b3d218043fc0932f9b83beb0705d
Peter Hoeg <peter@hoeg.com>
2024-04-16 18:47:23 +0200

    ctrlesc

--- users/peterhoeg/peterhoeg.c
+++ users/peterhoeg/peterhoeg.c
@@ -6,6 +6,8 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(2, KC_F):
             return 250;
+        case LCTL_T(KC_ESC):
+            return 150;
         default:
             return TAPPING_TERM;
     }
