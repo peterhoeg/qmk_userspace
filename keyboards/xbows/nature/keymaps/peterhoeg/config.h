d22bd295104be17a3fce0e4b3d87f15978a7f35e
Peter Hoeg <peter@hoeg.com>
2024-04-18 11:13:46 +0200

    improve tapping

--- keyboards/xbows/nature/keymaps/peterhoeg/config.h
+++ keyboards/xbows/nature/keymaps/peterhoeg/config.h
@@ -1,7 +1,10 @@
 #define LAYER_STATE_8BIT // we don't need more than 8 layers
 #define NO_MUSIC_MODE
 #define RGBLIGHT_LAYERS 2 // default is 8
-#define TAPPING_TERM 200  // default is 200
+
+// Tap-and-hold settings
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM 200 // default is 200
 #define TAPPING_TERM_PER_KEY
 
 // I don't have Cherry MX Lock switches
