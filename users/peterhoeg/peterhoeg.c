40d65e59224c04cc7ec476f068269a32a8a411de
Peter Hoeg <peter@hoeg.com>
2024-04-18 17:10:41 +0200

    prep for layer names

--- users/peterhoeg/peterhoeg.c
+++ users/peterhoeg/peterhoeg.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-/* enum layers { _BASE, _FIRST, _SECOND, _THIRD }; */
+enum layers { _BASE, _WINDOWS, _ARROWS, _NUMBERS };
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
