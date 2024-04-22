7d38b86e179f1a9263622b33e63e84959fb49783
Peter Hoeg <peter@hoeg.com>
2024-04-22 09:32:26 +0200

    trying permissive_hold

--- users/peterhoeg/peterhoeg.c
+++ users/peterhoeg/peterhoeg.c
@@ -10,11 +10,11 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         case C_ALTS:
         case C_SFTD:
         case C_CTLF:
-            return 250;
         case C_CTLJ:
         case C_SFTK:
         case C_ALTL:
         case C_GUIsc:
+            return TAPPING_TERM;
         case C_CTLes:
             return 150;
         default:
