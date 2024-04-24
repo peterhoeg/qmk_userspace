f59539ad85bf569ded61de3f8139731855c190d9
Peter Hoeg <peter@hoeg.com>
2024-04-24 21:50:59 +0200

    split files

--- keyboards/xbows/nature/keymaps/peterhoeg/keymap.c
+++ keyboards/xbows/nature/keymaps/peterhoeg/keymap.c
@@ -1,5 +1,13 @@
 #include QMK_KEYBOARD_H
 
+enum layers { _BASE, _WINDOWS, _ARROWS, _NUMBERS };
+
+#include "keymap_combo.c"
+
 #include "keymap_dk.c"
+
+#include "keymap_rgb.c"
+
+#include "keymap_tap.c"
 
 #include "keymap_generated.c"
