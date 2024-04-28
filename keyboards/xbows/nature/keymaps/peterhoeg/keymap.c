0348bef96fbb0470d9146cdd9ba8c9b4a5890da4
Peter Hoeg <peter@hoeg.com>
2024-04-28 15:21:50 +0200

    move to keymap.c

--- keyboards/xbows/nature/keymaps/peterhoeg/keymap.c
+++ keyboards/xbows/nature/keymaps/peterhoeg/keymap.c
@@ -1,13 +1,21 @@
 #include QMK_KEYBOARD_H
 
-enum layers { _BASE, _WINDOWS, _ARROWS, _NUMBERS };
+enum layers { _BASE, _WINDOWS, _ARROWS, _NUMBERS , _DANISH};
 
 #include "keymap_combo.c"
 
 #include "keymap_dk.c"
 
-#include "keymap_rgb.c"
+// #include "keymap_rgb.c"
 
 #include "keymap_tap.c"
 
+#define ___ KC_TRNS
+#define _x_ KC_NO
+
 #include "keymap_generated.c"
+
+/* Local Variables: */
+/* apheleia-inhibit: 't */
+/* +format-with-lsp: nil */
+/* End: */
