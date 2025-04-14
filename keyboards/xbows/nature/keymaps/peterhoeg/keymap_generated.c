49760d4f8bb4db049c0ae80a1b35b8f2337c4b3a
Peter H. Hoeg <peter@hoeg.com>
2025-04-14 09:26:05 +0200

    formatting

--- keyboards/xbows/nature/keymaps/peterhoeg/keymap_generated.c
+++ keyboards/xbows/nature/keymaps/peterhoeg/keymap_generated.c
@@ -1,7 +1,6 @@
 #if __has_include("keymap.h")
-#    include "keymap.h"
+#include "keymap.h"
 #endif
-
 
 /* THIS FILE WAS GENERATED!
  *
@@ -33,3 +32,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef OTHER_KEYMAP_C
 #include OTHER_KEYMAP_C
 #endif // OTHER_KEYMAP_C
+
