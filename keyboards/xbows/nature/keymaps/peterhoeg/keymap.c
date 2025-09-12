7f303a0773203a7f6c1bde6996f44845dbe978e0
Peter H. Hoeg <peter@hoeg.com>
2025-09-12 18:21:50 +0200

    chordal hold

--- keyboards/xbows/nature/keymaps/peterhoeg/keymap.c
+++ keyboards/xbows/nature/keymaps/peterhoeg/keymap.c
@@ -60,11 +60,10 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
 
 // this is temporary until I have it properly set up
 char chordal_hold_handedness(keypos_t key) {
-  /*
-  if (key.col == 0 || key.col == MATRIX_COLS - 1) {
-      return '*';  // Exempt the outer columns.
+  // exempt the left column and the last row
+  if (key.col == 0 || key.row == MATRIX_ROWS - 1) {
+    return '*';
   }
-  */
   // On split keyboards, typically, the first half of the rows are on the
   // left, and the other half are on the right.
   return key.col < MATRIX_COLS / 2 ? 'L' : 'R';
